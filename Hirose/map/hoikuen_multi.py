98068cef242c1a92c45ab272460a64c5cc03f504
NH252810 <nozomi_otani@hotmail.co.jp>
2023-03-01 14:27:17 +0000

    物件（新築マンション＆新築戸建てのみ）のプロット

--- Hirose/map/hoikuen_multi.py
+++ Hirose/map/hoikuen_multi.py
@@ -1,7 +1,7 @@
 import streamlit as st                                    # streamlit
 from streamlit_folium import st_folium                    # streamlitでfoliumを使う
 import folium                                             # folium
-# from folium import FeatureGroup                         # FeatureGrop
+from folium import FeatureGroup                           # FeatureGrop
 import pandas as pd                                       # CSVをデータフレームとして読み込む
 
 
@@ -58,5 +58,80 @@ for i, row in df2.iterrows():
         fill= True,
         color= 'red'
     ).add_to(map)
+
+
+
+
+
+# >>> 物件のプロット >>>
+bukken_df = pd.read_csv('bukken_0301.csv')
+
+new_mansion_data = bukken_df[bukken_df["category"].str.contains("新築マンション")]
+used_mansion_data = bukken_df[bukken_df["category"].str.contains("中古マンション")]
+new_kodate_data = bukken_df[bukken_df["category"].str.contains("新築一戸建て")]
+used_kodate_data = bukken_df[bukken_df["category"].str.contains("中古一戸建て")]
+
+
+# 新築マンションのプロット
+new_mansion_group = FeatureGroup(name="新築マンション")
+
+for i, row in new_mansion_data.iterrows():
+    html= f'''
+        <a href= '{row.loc['url']}' target='_blank'>SUUMOで確認</a>'''
+    iframe = folium.IFrame('【カテゴリー】: ' + row.loc['category'] + '<br>' + '【物件名】:' + row.loc['name']+ '<br>' + '【住所】:' + row.loc['adress']+'<br>' + '【最高価格】:' + str(row.loc['max_price'])+'<br>' + '【最低価格】:' + str(row.loc['min_price'])+'<br>' + '【最大面積】:' + str(row.loc['max_tatemono_measure'])+'<br>' + '【最小面積】:' + str(row.loc['min_tatemono_measure'])+'<br>' + '【最大間取り】:' + str(row.loc['max_madori'])+'<br>' + '【最小間取り】:' + str(row.loc['min_madori'])+'<br>' + '【最寄路線】:' + str(row.loc['nearest_line'])+'<br>' + '【最寄駅】:' + str(row.loc['nearest_station'])+'<br>' + '【最寄駅まで】:' + str(row.loc['walk_time'])+'<br>' + '【引渡可能時期】:' + str(row.loc['hikiwatashi']) +'<br>' + '【SUUMO】:' + str(row.loc['url']) )
+    popup = folium.Popup(iframe, min_width=300, max_width=300)
+    folium.Marker(
+        location= [row["latitude"], row["longitude"]],
+        popup= popup,
+        icon= folium.Icon(icon="home", prefix="fa", icon_color="white", color="purple")
+    ).add_to(new_mansion_group)
+
+new_mansion_group.add_to(map)
+
+
+# # 中古マンションのプロット
+# used_mansion_group = FeatureGroup(name="中古マンション")
+
+# for i, row in used_mansion_data.iterrows():
+#     iframe = folium.IFrame('【カテゴリー】: ' + row.loc['category'] + '<br>' + '【物件名】:' + row.loc['name']+ '<br>' + '【住所】:' + row.loc['adress']+'<br>' + '【最高価格】:' + str(row.loc['max_price'])+'<br>' + '【最低価格】:' + str(row.loc['min_price'])+'<br>' + '【最大面積】:' + str(row.loc['max_tatemono_measure'])+'<br>' + '【最小面積】:' + str(row.loc['min_tatemono_measure'])+'<br>' + '【最大間取り】:' + str(row.loc['max_madori'])+'<br>' + '【最小間取り】:' + str(row.loc['min_madori'])+'<br>' + '【最寄路線】:' + str(row.loc['nearest_line'])+'<br>' + '【最寄駅】:' + str(row.loc['nearest_station'])+'<br>' + '【最寄駅まで】:' + str(row.loc['walk_time'])+'<br>' + '【引渡可能時期】:' + str(row.loc['hikiwatashi']) +'<br>' + '【SUUMO】:' + str(row.loc['url']))
+#     popup = folium.Popup(iframe, min_width=300, max_width=300)
+#     folium.Marker(
+#         location= [row["latitude"], row["longitude"]],
+#         popup= popup,
+#         icon= folium.Icon(icon="home", prefix="fa", icon_color="white", color="pink")
+#     ).add_to(used_mansion_group)
+
+# used_mansion_group.add_to(map)
+
+
+#　新築一戸建てのプロット
+new_kodate_group = FeatureGroup(name="中古マンション")
+
+for i, row in new_kodate_data.iterrows():
+    iframe = folium.IFrame('【カテゴリー】: ' + row.loc['category'] + '<br>' + '【物件名】:' + row.loc['name']+ '<br>' + '【住所】:' + row.loc['adress']+'<br>' + '【最高価格】:' + str(row.loc['max_price'])+'<br>' + '【最低価格】:' + str(row.loc['min_price'])+'<br>' + '【最大面積】:' + str(row.loc['max_tatemono_measure'])+'<br>' + '【最小面積】:' + str(row.loc['min_tatemono_measure'])+'<br>' + '【最大間取り】:' + str(row.loc['max_madori'])+'<br>' + '【最小間取り】:' + str(row.loc['min_madori'])+'<br>' + '【最寄路線】:' + str(row.loc['nearest_line'])+'<br>' + '【最寄駅】:' + str(row.loc['nearest_station'])+'<br>' + '【最寄駅まで】:' + str(row.loc['walk_time'])+'<br>' + '【引渡可能時期】:' + str(row.loc['hikiwatashi']) +'<br>' + '【SUUMO】:' + str(row.loc['url']))
+    popup = folium.Popup(iframe, min_width=300, max_width=300)
+    folium.Marker(
+        location= [row["latitude"], row["longitude"]],
+        popup= popup,
+        icon= folium.Icon(icon="home", prefix="fa", icon_color="white", color="orange")
+    ).add_to(new_kodate_group)
+
+new_kodate_group.add_to(map)
+
+
+# #　中古一戸建てのプロット
+# used_kodate_group = FeatureGroup(name="中古マンション")
+
+# for i, row in new_kodate_data.iterrows():
+#     iframe = folium.IFrame('【カテゴリー】: ' + row.loc['category'] + '<br>' + '【物件名】:' + row.loc['name']+ '<br>' + '【住所】:' + row.loc['adress']+'<br>' + '【最高価格】:' + str(row.loc['max_price'])+'<br>' + '【最低価格】:' + str(row.loc['min_price'])+'<br>' + '【最大面積】:' + str(row.loc['max_tatemono_measure'])+'<br>' + '【最小面積】:' + str(row.loc['min_tatemono_measure'])+'<br>' + '【最大間取り】:' + str(row.loc['max_madori'])+'<br>' + '【最小間取り】:' + str(row.loc['min_madori'])+'<br>' + '【最寄路線】:' + str(row.loc['nearest_line'])+'<br>' + '【最寄駅】:' + str(row.loc['nearest_station'])+'<br>' + '【最寄駅まで】:' + str(row.loc['walk_time'])+'<br>' + '【引渡可能時期】:' + str(row.loc['hikiwatashi']) +'<br>' + '【SUUMO】:' + str(row.loc['url']))
+#     popup = folium.Popup(iframe, min_width=300, max_width=300)
+#     folium.Marker(
+#         location= [row["latitude"], row["longitude"]],
+#         popup= popup,
+#         icon= folium.Icon(icon="home", prefix="fa", icon_color="white", color="beige")
+#     ).add_to(used_kodate_group)
+
+# used_kodate_group.add_to(map)
+
 
 st_folium(map, width=700, height=700)
